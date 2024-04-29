a332bdd509b1229ee225b494df7f4f96d6d24e8b
Abtin Mogharabin <atbinmogharabin@gmail.com>
2024-04-29 19:16:24 +0300

    Update AS_Shiny.R

--- Shiny app/AS_Shiny.R
+++ Shiny app/AS_Shiny.R
@@ -1,14 +1,8 @@
-
-
 #Import Libraries
-
 source(file.path("utils", "LibReader.R"),  local = TRUE)$value
 
-
 #Import Data
-
 source(file.path("utils", "DataReader.R"),  local = TRUE)$value
-
 
 button_color_css <- "
 #DivCompClear, #FinderClear, #EnterTimes{
@@ -19,34 +13,24 @@ background: DodgerBlue;
 font-size: 15px;
 }"
 
-
 ui <- fluidPage(
   #Navbar structure for UI
   navbarPage("AS-Shiny", theme = shinytheme("flatly"), #theme = shinytheme("superhero")
-             
              source(file.path("ui", "About.R"),  local = TRUE)$value,
              source(file.path("ui", "LocationFinder.R"),  local = TRUE)$value,
              source(file.path("ui", "DataVisualization.R"),  local = TRUE)$value,
              source(file.path("ui", "ModelAnalysis.R"),  local = TRUE)$value,
              source(file.path("ui", "DataExplorer.R"),  local = TRUE)$value,
              source(file.path("ui", "Questions.R"),  local = TRUE)$value
-             
              )
   )
-                          
-           
 
 server <- function(input, output) {
-    
-  
   source(file.path("server", "LocationFinder.R"),  local = TRUE)$value
   source(file.path("server", "DataVisualization.R"),  local = TRUE)$value
   source(file.path("server", "ModelAnalysis.R"),  local = TRUE)$value
   source(file.path("server", "DataExplorer.R"),  local = TRUE)$value
-  
-
 }
-
 
 # Run the application
 shinyApp(ui = ui, server = server)
