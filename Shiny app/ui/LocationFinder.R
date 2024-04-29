13961ac6bed3ee69a6371fb0156f37e17050610b
Abtin Mogharabin <atbinmogharabin@gmail.com>
2024-04-29 19:24:09 +0300

    Update LocationFinder.R

--- Shiny app/ui/LocationFinder.R
+++ Shiny app/ui/LocationFinder.R
@@ -1,12 +1,9 @@
 tabPanel('Location Finder', icon = icon("globe-americas"),
          headerPanel(''),
-         
          # Input values
          sidebarPanel(
            h3(strong('MAP')),
-           
-           
-           
+                  
            selectInput("country1", label = "Country (if available):", 
                        choices = countrylist,
                        selected =1,
@@ -24,19 +21,17 @@ tabPanel('Location Finder', icon = icon("globe-americas"),
                                                   "Total decisions" = "Total.decisions",
                                                   "Total pending at the end of year" = "Total.persons.pending.end.year",
                                                   "Of which UNHCR assisted" = "of.which.UNHCR.assisted.1"),
-                                   selected =1))),
-           
-           
+                                   selected =1)
+                   )
+           ),
            sliderInput("year1", "Year:",
                        min = 2000, max = 2014,
                        value = 2000, sep = ""),
-           
            actionButton("button", "Submit", icon = icon("play"), class = "btn btn-primary"), width = 3
          ),
-         
          mainPanel(
            tags$label(h3(strong("Let's explore the map!"))),
            tags$div(class = "widget-user-header bg-red-active text-center"),
            leafletOutput("mymap", height = "500"),# Status/Output Text Box
-           
-         ))
+         )
+        )
